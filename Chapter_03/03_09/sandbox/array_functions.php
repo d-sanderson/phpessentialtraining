a90230f3b1b8104e0a010f92bdc142528eec85d6
dsanderson90 <dsanderson3@cnm.edu>
2019-04-11 15:01:02 -0600

    finished section on array functions

--- Chapter_03/03_09/sandbox/array_functions.php
+++ Chapter_03/03_09/sandbox/array_functions.php
@@ -12,18 +12,31 @@
     Count:         <?php echo count($numbers); ?><br />
     Max value:     <?php echo max($numbers);   ?><br />
     Min value:     <?php echo min($numbers);   ?><br />
+    Last Value:     <?php echo end($numbers);   ?><br />
     <br />
     <pre>
+    <!-- the sort function sorts the $numbers array in place meaning $numbers = sort($numbers) is not necessary -->
+    <!-- it also overrides the old numbers array and returns the sorted version -->
     Sort:          <?php sort($numbers);  print_r($numbers); ?><br />
     Reverse sort: <?php rsort($numbers); print_r($numbers); ?><br />
     </pre>
     <br />
+    <!-- Turns an array in a string -->
     Implode:       <?php echo $num_string = implode(" * ", $numbers); ?><br />
+    <!-- Turns a string into an array separated by value in first argument -->
     Explode:       <?php print_r(explode(" * ", $num_string)); ?><br />
     <br />
     
+    <!-- Returns 1 if true, nothing if false -->
     15 in array?: <?php echo in_array(15, $numbers); // returns T/F ?><br />
     19 in array?: <?php echo in_array(19, $numbers); // returns T/F ?><br />
+
+    <!-- REFERENCE https://www.php.net/manual/en/function.array-chunk.php -->
+    <!-- array_push adds an element to the end of the array  
+         array_pop removes an element from the end of an array
+         array_shift adds an element to the beginning of the array
+         array_unshift removes an element from the end of the array -->
+    
     
 
   </body>
