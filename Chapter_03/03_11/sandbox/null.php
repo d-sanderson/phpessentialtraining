b5b53a58fd8a2746ca620eb4b4e29e0a0c2d3e23
dsanderson90 <dsanderson3@cnm.edu>
2019-04-11 15:10:36 -0600

    reviewing NULL and empty in PHP

--- Chapter_03/03_11/sandbox/null.php
+++ Chapter_03/03_11/sandbox/null.php
@@ -8,6 +8,8 @@
   <body>
 
     <?php
+    // Like booleans null is case-insensitive NULL or null can be used interchangeably.
+    // null means a variable is lacking a value, undefined means that a variable has not been assigned a value.
       $var1 = null;
       $var2 = "";
     ?>
@@ -15,12 +17,13 @@
     var2 null? <?php echo is_null($var2); ?><br />
     var3 null? <?php echo is_null($var3); ?><br />
     <br />
+    <!-- Has a variable been declared? isset checks this -->
     var1 is set? <?php echo isset($var1); ?><br />
     var2 is set? <?php echo isset($var2); ?><br />
     var3 is set? <?php echo isset($var3); ?><br />
     <br />
     
-    <?php // empty: "", null, 0, 0.0, "0", false, array() ?>
+    <?php // empty: returns a boolean for whether a variable is empty. In PHP, empty can be any of these: "", null, 0, 0.0, "0", false, array() ?>
     
     <?php $var3 = "0"; ?>
     var1 empty? <?php echo empty($var1); ?><br />
