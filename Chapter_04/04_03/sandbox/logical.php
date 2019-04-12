9c5c1bfa9aca3b21a0c8a28e8dc8ad5f5aa335cf
dsanderson90 <dsanderson3@cnm.edu>
2019-04-11 22:30:10 -0600

    learned logical operators and how to use an if statement to set a value if it is not set.

--- Chapter_04/04_03/sandbox/logical.php
+++ Chapter_04/04_03/sandbox/logical.php
@@ -12,11 +12,13 @@
       $b = 4;
       
       if ($a > $b) {
-        echo "a is larger than b";
-      } elseif ($a < $b) {
-        echo "a is smaller than b";
-      } else {
-        echo "a is equal to b";
+        echo "a is larger than b.";
+      }
+      elseif ($a < $b) {
+        echo "a is smaller than b.";
+      }
+      else {
+        echo "a is equal to b.";
       }
 
     
